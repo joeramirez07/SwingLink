ba20b8b38eab400764912e0d8655ed1b0180453d
joeramirez07 <joeramirez07@gmail.com>
2025-06-10 13:08:25 -0500

    Cleanup code

--- backend/models/user.js
+++ backend/models/user.js
@@ -15,7 +15,6 @@ const userSchema = new Schema(
       required: true,
     },
     phoneNumber: {
-      // NEW: For SMS notifications
       type: String,
       required: true,
     },
@@ -24,7 +23,6 @@ const userSchema = new Schema(
       required: true,
     },
     inviteCode: {
-      // NEW: For joining golf groups
       type: String,
       unique: true,
       uppercase: true,
@@ -32,7 +30,7 @@ const userSchema = new Schema(
   },
   {
     timestamps: true,
-    // Remove password when doc is sent across network
+
     toJSON: {
       transform: function (doc, ret) {
         delete ret.password;
@@ -42,14 +40,11 @@ const userSchema = new Schema(
   },
 );
 
-// Generate invite code for new users
 userSchema.pre("save", async function (next) {
-  // Generate unique invite code for new users
   if (this.isNew && !this.inviteCode) {
     this.inviteCode = Math.random().toString(36).substring(2, 8).toUpperCase();
   }
 
-  // Hash password if modified
   if (!this.isModified("password")) return next();
   this.password = await bcrypt.hash(this.password, SALT_ROUNDS);
   next();
--- backend/server.js
+++ backend/server.js
@@ -1,32 +1,20 @@
-const path = require("path"); // Built into Node
+const path = require("path"); 
 const express = require("express");
 const logger = require("morgan");
 const app = express();
 
-// Process the secrets/config vars in .env
-require("dotenv").config();
 
-// Connect to the database
+require("dotenv").config();
 require("./db");
 
 app.use(logger("dev"));
-// Serve static assets from the frontend's built code folder (dist)
 app.use(express.static(path.join(__dirname, "../frontend/dist")));
-// Note that express.urlencoded middleware is not needed
-// because forms are not submitted!
 app.use(express.json());
-
-// Middleware to check the request's headers for a JWT and
-// verify that it's a valid.  If so, it will assign the
-// user object in the JWT's payload to req.user
 app.use(require("./middleware/checkToken"));
-
-// API Routes
 app.use("/api/auth", require("./routes/auth"));
 app.use("/api/posts", require("./routes/posts"));
 app.use("/api/groups", require("./routes/groups"));
 
-// Use a "catch-all" route to deliver the frontend's production index.html
 app.get("/*splat", function (req, res) {
   res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
 });
