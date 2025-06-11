5f784343ec6e65afe9be021ebeaff0eef647ad02
joeramirez07 <joeramirez07@gmail.com>
2025-06-11 15:56:48 -0500

    outings.js in my routes complete

---
+++ backend/routes/outings.js
@@ -0,0 +1,13 @@
+
+const express = require('express');
+const router = express.Router();
+const outingsCtrl = require('../controllers/outings');
+const ensureLoggedIn = require('../middleware/ensureLoggedIn');
+
+
+router.post('/:groupId/outings', ensureLoggedIn, outingsCtrl.create);
+
+
+router.post('/outings/:outingId/rsvp', ensureLoggedIn, outingsCtrl.addRsvp);
+
+module.exports = router;
