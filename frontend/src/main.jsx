3539b9f58a2ff392ea7d5a434a40473af63e207c
joeramirez07 <joeramirez07@gmail.com>
2025-06-13 14:49:45 -0500

    landing page css agian

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter as Router } from "react-router-dom";
 import "./index.css";
-import "./styles/styles.css";
+
 
 import App from "./pages/App/App";
 
--- frontend/src/pages/HomePage/HomePage.jsx
+++ frontend/src/pages/HomePage/HomePage.jsx
@@ -1,4 +1,5 @@
 import { useNavigate } from "react-router-dom";
+import "./HomePage.css"; 
 
 export default function HomePage({ user }) {
   const navigate = useNavigate();
